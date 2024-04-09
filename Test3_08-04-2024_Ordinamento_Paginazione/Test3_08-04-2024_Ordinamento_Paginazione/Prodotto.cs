5687d85c635427a2b71cfdfa7de81bb5a829ea15
Riccardo Carluccio <riccardo.carluccio.93@gmail.com>
2024-04-09 21:57:18 +0200

    Add files via upload

---
+++ Test3_08-04-2024_Ordinamento_Paginazione/Test3_08-04-2024_Ordinamento_Paginazione/Prodotto.cs
@@ -0,0 +1,10 @@
+﻿namespace Test3_08_04_2024_Ordinamento_Paginazione
+{
+    public class Prodotto
+    {
+        public int Id { get; set; }
+        public String? Nome { get; set; }
+        public String? Categoria { get; set; }
+        public double Prezzo { get; set; }
+    }
+}
